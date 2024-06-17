a51c43747b7ba6944a1df8c8cf30c1fedb037ec0
GGore369 <118764332+GGore369@users.noreply.github.com>
2024-06-17 19:20:13 +0530

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,126 @@
+// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '70a3ddcd9fmshcf44298b3fd41f0p1fb8f8jsne76831d7be21',
+// 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+// fetch(url,options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+
+
+function rada(city) {
+
+    let searchCity = document.getElementById('search-city')
+    let cityName = document.getElementById("city-name")
+
+    cityName.innerHTML = city
+    searchCity.value = ""
+
+    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '70a3ddcd9fmshcf44298b3fd41f0p1fb8f8jsne76831d7be21',
+            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+        }
+    };
+
+
+    async function my_weather_app() {
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result)
+            temp.innerHTML = result.temp
+            max_temp.innerHTML = result.max_temp
+            min_temp.innerHTML = result.min_temp
+            // cloud_pct.innerHTML = result.cloud_pct
+            feels_like.innerHTML = result.feels_like
+            humidity.innerHTML = result.humidity
+            wind_speed.innerHTML = result.wind_speed
+            // sunrise.innerHTML = result.sunrise
+            // sunset.innerHTML = result.sunset
+            wind_degrees.innerHTML = result.wind_degrees
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    my_weather_app();
+
+}
+
+let searchGlass = document.getElementsByClassName('search-glass')[0];
+searchGlass.addEventListener('click',(event)=>{
+    let searchCity = document.getElementById('search-city')
+    rada(searchCity.value)
+})
+
+rada("Mumbai");
+
+
+
+let date = new Date();
+
+let year = date.getFullYear();
+console.log(year);
+
+const month = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
+
+
+let gateDate = document.getElementById('getdate')
+
+function Time(){
+    let date = new Date();
+
+    let y = date.getFullYear();
+    let mon = date.getMonth();
+    let m = month[mon];
+    let d = date.getDate();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    
+
+    seconds = parseInt(seconds)
+    minutes = parseInt(minutes)
+    hour = parseInt(hour)
+
+    if(d < 10){
+        d = '0' + d;
+    }
+
+
+    if(minutes < 10){
+        minutes = '0' + parseInt(minutes)
+    }
+
+    if(hour < 10){
+        hour = '0' + parseInt(hour)
+        moraft.innerHTML = "AM"
+    }
+
+    if(hour > 12){
+        hour = hour - 12;
+        hour = '0' + hour;
+        moraft.innerHTML = "PM"
+    }
+
+    getdate.innerHTML = d;
+    getmonth.innerHTML = m; 
+    getyear.innerHTML = y;
+
+    gethour.innerHTML = hour;
+    getminute.innerHTML = minutes;
+
+}
+
+setInterval(Time,1000);
